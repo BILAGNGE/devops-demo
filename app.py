1cb79e124fb672ea5b7bca77e5b726ecdc2e98f8
BILAGNGE <bilagnge@gmail.com>
2026-03-31 12:16:49 +0200

    TP5 - tests unitaires + ci-cd.yml

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 from flask import Flask
-import os
-
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    message = os.getenv("MESSAGE", "Hello Default")
-    return message
+@app.route('/')
+def hello():
+    return "Hello DevOps"
 
-app.run(host="0.0.0.0", port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
